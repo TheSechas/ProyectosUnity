bf749c6f4d059f5bc123babc2d70f9af9b5599b7
Sergio González García <90698977+TheSechas@users.noreply.github.com>
2024-04-08 13:38:54 +0200

    Update MouseLook.cs

--- MouseLook.cs
+++ MouseLook.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
@@ -15,12 +17,12 @@ public class MouseLook : MonoBehaviour
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quarternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX)
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
