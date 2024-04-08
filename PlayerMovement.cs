166c8692acb150af955fe19a8559d54fb99232a8
Sergio González García <90698977+TheSechas@users.noreply.github.com>
2024-04-08 11:39:40 +0200

    Movement and Camera FirstPerson
    Creacion de script de movimiento y de la cámara en primera persona en C#

---
+++ PlayerMovement.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeigh = 3f;
+
+    public Transform gorundCheck;
+    public float gorundDistance = 0,4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    void Start()
+        {
+
+        }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(gorundCheck.position, gorundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal")
+        float y = Input.GetAxis("Vertical")
+
+        Vector3 move = transform.right * x + transform.foward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtomDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeigh * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
